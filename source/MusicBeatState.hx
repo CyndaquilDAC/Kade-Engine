8f7fdbbcd9e8520fd99aa540455e684bd4c4cfca
CyndaquilDAC <65635932+CyndaquilDAC@users.noreply.github.com>
2021-07-18 18:03:13 -0500

    music beat shit

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -1,6 +1,8 @@
 package;
 
+#if windows
 import Discord.DiscordClient;
+#end
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import openfl.Lib;
@@ -10,107 +12,125 @@ import flixel.addons.transition.FlxTransitionableState;
 import flixel.addons.ui.FlxUIState;
 import flixel.math.FlxRect;
 import flixel.util.FlxTimer;
-import flixel.*;
-import haxe.*;
-import lime.*;
-import openfl.*;
 
 class MusicBeatState extends FlxUIState
 {
-	private var lastBeat:Float = 0;
-	private var lastStep:Float = 0;
+    private var lastBeat:Float = 0;
+    private var lastStep:Float = 0;
 
-	private var curStep:Int = 0;
-	private var curBeat:Int = 0;
-	private var controls(get, never):Controls;
+    private var curStep:Int = 0;
+    private var curBeat:Int = 0;
+    private var controls(get, never):Controls;
 
-	inline function get_controls():Controls
-		return PlayerSettings.player1.controls;
+    inline function get_controls():Controls
+        return PlayerSettings.player1.controls;
 
-	override function create()
-	{
-		(cast (Lib.current.getChildAt(0), Main)).setFPSCap(FlxG.save.data.fpsCap);
+    override function create()
+    {
+        (cast (Lib.current.getChildAt(0), Main)).setFPSCap(FlxG.save.data.fpsCap);
 
-		if (transIn != null)
-			trace('reg ' + transIn.region);
+        if (transIn != null)
+            trace('reg ' + transIn.region);
 
-		super.create();
-	}
+        super.create();
+    }
 
 
-	var array:Array<FlxColor> = [
-		FlxColor.fromRGB(148, 0, 211),
-		FlxColor.fromRGB(75, 0, 130),
-		FlxColor.fromRGB(0, 0, 255),
-		FlxColor.fromRGB(0, 255, 0),
-		FlxColor.fromRGB(255, 255, 0),
-		FlxColor.fromRGB(255, 127, 0),
-		FlxColor.fromRGB(255, 0 , 0)
-	];
+    var array:Array<FlxColor> = [
+        FlxColor.fromRGB(148, 0, 211),
+        FlxColor.fromRGB(75, 0, 130),
+        FlxColor.fromRGB(0, 0, 255),
+        FlxColor.fromRGB(0, 255, 0),
+        FlxColor.fromRGB(255, 255, 0),
+        FlxColor.fromRGB(255, 127, 0),
+        FlxColor.fromRGB(255, 0 , 0)
+    ];
 
-	var skippedFrames = 0;
+    var skippedFrames = 0;
 
-	override function update(elapsed:Float)
-	{
-		//everyStep();
-		var oldStep:Int = curStep;
+    override function update(elapsed:Float)
+    {
+        //everyStep();
+        var nextStep:Int = updateCurStep();
 
-		updateCurStep();
-		updateBeat();
+        if (nextStep >= 0)
+        {
+            if (nextStep > curStep)
+            {
+                for (i in curStep...nextStep)
+                {
+                    curStep++;
+                    updateBeat();
+                    stepHit();
+                }
+            }
+            else if (nextStep < curStep)
+            {
+                //Song reset?
+                curStep = nextStep;
+                updateBeat();
+                stepHit();
+            }
+        }
 
-		if (oldStep != curStep && curStep > 0)
-			stepHit();
+        if (FlxG.save.data.fpsRain && skippedFrames >= 6)
+            {
+                if (currentColor >= array.length)
+                    currentColor = 0;
+                (cast (Lib.current.getChildAt(0), Main)).changeFPSColor(array[currentColor]);
+                currentColor++;
+                skippedFrames = 0;
+            }
+            else
+                skippedFrames++;
 
-		if (FlxG.save.data.fpsRain && skippedFrames >= 6)
-			{
-				if (currentColor >= array.length)
-					currentColor = 0;
-				(cast (Lib.current.getChildAt(0), Main)).changeFPSColor(array[currentColor]);
-				currentColor++;
-				skippedFrames = 0;
-			}
-			else
-				skippedFrames++;
+        if ((cast (Lib.current.getChildAt(0), Main)).getFPSCap != FlxG.save.data.fpsCap && FlxG.save.data.fpsCap <= 290)
+            (cast (Lib.current.getChildAt(0), Main)).setFPSCap(FlxG.save.data.fpsCap);
 
-		if ((cast (Lib.current.getChildAt(0), Main)).getFPSCap != FlxG.save.data.fpsCap)
-			(cast (Lib.current.getChildAt(0), Main)).setFPSCap(FlxG.save.data.fpsCap);
+        super.update(elapsed);
+    }
 
-		super.update(elapsed);
-	}
+    private function updateBeat():Void
+    {
+        lastBeat = curBeat;
+        curBeat = Math.floor(curStep / 4);
+    }
 
-	private function updateBeat():Void
-	{
-		lastBeat = curStep;
-		curBeat = Math.floor(curStep / 4);
-	}
+    public static var currentColor = 0;
 
-	public static var currentColor = 0;
+    private function updateCurStep():Int
+    {
+        var lastChange:BPMChangeEvent = {
+            stepTime: 0,
+            songTime: 0,
+            bpm: 0
+        }
+        for (i in 0...Conductor.bpmChangeMap.length)
+        {
+            if (Conductor.songPosition >= Conductor.bpmChangeMap[i].songTime)
+                lastChange = Conductor.bpmChangeMap[i];
+        }
 
-	private function updateCurStep():Void
-	{
-		var lastChange:BPMChangeEvent = {
-			stepTime: 0,
-			songTime: 0,
-			bpm: 0
-		}
-		for (i in 0...Conductor.bpmChangeMap.length)
-		{
-			if (Conductor.songPosition >= Conductor.bpmChangeMap[i].songTime)
-				lastChange = Conductor.bpmChangeMap[i];
-		}
+        return lastChange.stepTime + Math.floor((Conductor.songPosition - lastChange.songTime) / Conductor.stepCrochet);
+    }
 
-		curStep = lastChange.stepTime + Math.floor((Conductor.songPosition - lastChange.songTime) / Conductor.stepCrochet);
-	}
+    public function stepHit():Void
+    {
+        if (curStep % 4 == 0)
+            beatHit();
+    }
 
-	public function stepHit():Void
-	{
+    public function beatHit():Void
+    {
+        //do literally nothing dumbass
+    }
 
-		if (curStep % 4 == 0)
-			beatHit();
-	}
-
-	public function beatHit():Void
-	{
-		//do literally nothing dumbass
-	}
+    public function fancyOpenURL(schmancy:String)
+    {
+        #if linux
+        Sys.command('/usr/bin/xdg-open', [schmancy, "&"]);
+        #else
+        FlxG.openURL(schmancy);
+        #end
+    }
 }
