12d35de74a27c7a3b54b83bd7ec95eb5fad756bd
CyndaquilDAC <65635932+CyndaquilDAC@users.noreply.github.com>
2021-08-11 20:51:42 -0500

    dwnwfeklkwfelwfeklwfeknlwfeklwfeklfwekfwekwefklwfeklwefklfwkwfelkfweklwfeklweklfwklefnwekflklwef

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -53,47 +53,6 @@ class TitleState extends MusicBeatState
 
 	var wackyImage:FlxSprite;
 
-	public var filters:Array<BitmapFilter> = [];
-
-    function ShaderFilters():Void
-	{
-        for(filter in filters)
-        {
-            filters.remove(filter);
-        }
-		//Matrix shaders:
-		if (FlxG.save.data.colorblindMode == 'deuteranopia')
-		{
-			var matrix:Array<Float> = [
-						0.43, 0.72, -.15, 0, 0,
-						0.34, 0.57, 0.09, 0, 0,
-						-.02, 0.03,    1, 0, 0,
-						   0,    0,    0, 1, 0,
-					];
-			filters.push(new ColorMatrixFilter(matrix));
-		}
-		if (FlxG.save.data.colorblindMode == 'protanopia')
-		{
-			var matrix:Array<Float> = [
-						0.20, 0.99, -.19, 0, 0,
-						0.16, 0.79, 0.04, 0, 0,
-						0.01, -.01,    1, 0, 0,
-						   0,    0,    0, 1, 0,
-					];
-			filters.push(new ColorMatrixFilter(matrix));
-		}
-		if (FlxG.save.data.colorblindMode == 'tritanopia')
-		{
-			var matrix:Array<Float> = [
-						0.20, 0.99, -.19, 0, 0,
-						0.16, 0.79, 0.04, 0, 0,
-						0.01, -.01,    1, 0, 0,
-						   0,    0,    0, 1, 0,
-					];
-			filters.push(new ColorMatrixFilter(matrix));
-		}
-	}
-
 	override public function create():Void
 	{
 		#if polymod
@@ -119,7 +78,9 @@ class TitleState extends MusicBeatState
 		SaveHandler.initSave();
 		
 		#end
-				
+		
+
+
 		Highscore.load();
 
 
@@ -484,6 +445,47 @@ class TitleState extends MusicBeatState
 	}
 
 	var skippedIntro:Bool = false;
+
+	public var filters:Array<BitmapFilter> = [];
+
+    function ShaderFilters():Void
+	{
+        for(filter in filters)
+        {
+            filters.remove(filter);
+        }
+		//Matrix shaders:
+		if (FlxG.save.data.colorblindMode == 'deuteranopia')
+		{
+			var matrix:Array<Float> = [
+						0.43, 0.72, -.15, 0, 0,
+						0.34, 0.57, 0.09, 0, 0,
+						-.02, 0.03,    1, 0, 0,
+						   0,    0,    0, 1, 0,
+					];
+			filters.push(new ColorMatrixFilter(matrix));
+		}
+		if (FlxG.save.data.colorblindMode == 'protanopia')
+		{
+			var matrix:Array<Float> = [
+						0.20, 0.99, -.19, 0, 0,
+						0.16, 0.79, 0.04, 0, 0,
+						0.01, -.01,    1, 0, 0,
+						   0,    0,    0, 1, 0,
+					];
+			filters.push(new ColorMatrixFilter(matrix));
+		}
+		if (FlxG.save.data.colorblindMode == 'tritanopia')
+		{
+			var matrix:Array<Float> = [
+						0.20, 0.99, -.19, 0, 0,
+						0.16, 0.79, 0.04, 0, 0,
+						0.01, -.01,    1, 0, 0,
+						   0,    0,    0, 1, 0,
+					];
+			filters.push(new ColorMatrixFilter(matrix));
+		}
+	}
 
 	function skipIntro():Void
 	{
